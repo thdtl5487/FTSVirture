1c72fabf67023bd26466ba17eb239b35fe151130
thdtl5487 <thdtl5487@gmail.com>
2022-08-26 16:39:14 +0900

    게시판 리스트 페이징 작업 완료

--- src/main/java/com/springboot/react/cboard/CBoardRepository.java
+++ src/main/java/com/springboot/react/cboard/CBoardRepository.java
@@ -1,14 +1,24 @@
 package com.springboot.react.cboard;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.NonUniqueResultException;
+import javax.persistence.OrderBy;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import com.springboot.react.cboard.CBoardVO;
 
@@ -16,12 +26,14 @@ import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
-public class CBoardRepository {
+public class CBoardRepository{
 	
 	// @PersistenceContext : JPA의 ORM을 처리해주는 EntityManager을 불러올 때 쓰는 애노테이션 입니다.
 	@PersistenceContext	
 	private final EntityManager em;
 	
+	@Autowired
+	CBoardRepositoryInterface cboardRepository;
 
 	public void insert(CBoardVO vo) {
 		// em.persist : JPA를 통해 값을 입력할 때 활용합니다.
@@ -52,13 +64,19 @@ public class CBoardRepository {
 	
 	public List<CBoardVO> getList(CBoardVO vo){
 	      
-	      List<CBoardVO> getList = em.createQuery("select b from CBoardVO b").getResultList();
-	      System.out.println(getList.get(1).getBtitle());
-	      System.out.println(getList.get(2).getBtitle());
-	      System.out.println(getList.get(3).getBtitle());
-	      System.out.println(getList.get(4).getBtitle());
-	      
-	      return getList;
+//		List getList = em.createQuery("select b from CBoardVO b").setFirstResult(0).setMaxResults(9).getResultList();
+		Sort sortNum = Sort.by("BNum").descending();
+		Pageable pageable = PageRequest.of(0, 10, sortNum);
+		
+		Page<CBoardVO> getList = cboardRepository.findAll(pageable);
+		
+		List<CBoardVO> result = new ArrayList<CBoardVO>();
+		
+		if(getList != null && getList.hasContent()) {
+			result = getList.getContent();
+		}
+		
+		return result;
 	   }
 	   
 	   
